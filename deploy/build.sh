0208502036ea7bf1eac4408ead160e5f82b88b68
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-10 17:49:48 +0800

    log

--- deploy/build.sh
+++ deploy/build.sh
@@ -12,8 +12,9 @@ remote_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$remote_dir/common.sh"
 
 
-log_step "check out source code"
+log_step "check out source code from $deploy_path"
 cd "$deploy_path"
+echo `pwd`
 
 git reset --hard
 git fetch 
@@ -34,28 +35,31 @@ log_step "Build image with Buildah"
 cd "$project_path"
 
 
+
 if [ "${1:-}" = "base" ]; then
-    log_step "🚀 Building base image..."
+    log_step "🚀 buildah building base image  $ui_image_base ..."
     buildah bud -t "$ui_image_base" -f deploy/Dockerfile.base
     buildah push --tls-verify=false "$ui_image_base" "docker://${ui_image_base}"
     log_step "✅ Base image built and pushed!"
 fi
 
 mv "$deploy_path/.current_tag_ui" "$deploy_path/.previous_tag_ui"
+log_step "🚀 buildah building latest image  $ui_image ..."
 buildah bud -t "$ui_image" -f deploy/Dockerfile .
 
 log_step "Record current git commit as the deployment tag"
 git rev-parse --short HEAD > "$deploy_path/.current_tag_ui"
 cat "$deploy_path/.current_tag_ui"
 
-log_step "Push image to registry"
+log_step "buildah push image to registry ${ui_image}"
 buildah push --tls-verify=false \
     "${ui_image}" "docker://${ui_image}"
 
-log_info "build-ui complete on k8master. Image: $ui_image"
 
+log_info "kubectl delete pod -l app=$module"
 kubectl delete pod -l app=$module
 
+log_info "build-ui complete on ${HOST}. Image: $ui_image"
 
 
 log_time START_TIME
