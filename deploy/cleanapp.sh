f5b637f41a8654b910fc93bcb16fb4df01f0bc5a
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-14 21:20:22 +0800

    cleanapp.sh

---
+++ deploy/cleanapp.sh
@@ -0,0 +1,45 @@
+
+set -exuo pipefail
+
+# If no parameters, restart all
+if [ $# -eq 0 ]; then
+    echo "which app to be cleaned up "
+    exit 0
+fi
+
+remote_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$remote_dir/common.sh"
+
+rm /data/logs/$module/* || true
+
+logfile=$(get_caller_script)
+start_log_file $logfile
+
+mylog "check out source code from $project_path"
+cd "$project_path"
+
+checkout
+
+check_status
+
+module=$1
+service="$module-svc"
+
+kubectl scale deployment $module --replicas=0 || true
+
+sleep 2
+
+kubectl delete svc $service || true
+
+kubectl delete deployment $module || true
+
+kubectl delete rs   -l app=$module || true
+kubectl delete pods -l app=$module || true
+kubectl delete all  -l app=$module || true
+
+kubectl get deploy,svc | grep -E "$module" || true
+
+check_status
+
+
+log_time
